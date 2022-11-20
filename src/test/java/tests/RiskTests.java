42901d5bbea55bde18f48cbfc6fcde034869a05f
nadia.trfm <69869414+Nadia-ru@users.noreply.github.com>
2022-11-20 18:21:28 +0300

    #3 add tests

--- src/test/java/tests/RiskTests.java
+++ src/test/java/tests/RiskTests.java
@@ -1,22 +1,45 @@
 package tests;
 
+import com.gitlab.nadiatrf.models.IRisk;
 import com.gitlab.nadiatrf.models.Risk;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.Arrays;
+import java.util.List;
 
+import static junit.framework.TestCase.assertEquals;
 
+@RunWith(MockitoJUnitRunner.class)
 public class RiskTests {
+    @Mock
+    IRisk risk;
 
-//    // проверяет равномерно ли распределилилсь риски по общей сумме страховки
+    // проверяет равномерно ли распределилилсь риски по общей сумме страховки
     @Test
-    public void testUseSum(){
-
+    public void testUseSum() {
+        Integer sum = 16000;
+        assertEquals("the risks do not match the distribution",
+                Arrays.asList(new Risk(4000), new Risk(4000), new Risk(4000), new Risk(4000)),
+                risk.useSum(sum));
     }
 
     @Test
-    public void verificationTest(){
+    public void testUseSumWhereSumIsZero(){
+        Mockito.doThrow(new RuntimeException()).when(risk).useSum(0);
 
+       risk.useSum(0);
+    }
+
+    @Test
+    public void testUseSumWhereNegativeSum(){
+        Mockito.doThrow(new RuntimeException()).when(risk).useSum(-1);
+
+        risk.useSum(-1);
     }
 }
