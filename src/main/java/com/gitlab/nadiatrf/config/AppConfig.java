9559a119216c8d3494f542fc0516c4b6265a94c8
nadia.trfm <69869414+Nadia-ru@users.noreply.github.com>
2022-11-26 18:31:17 +0300

    #2 подключение к бд

--- src/main/java/com/gitlab/nadiatrf/config/AppConfig.java
+++ src/main/java/com/gitlab/nadiatrf/config/AppConfig.java
@@ -1,11 +1,25 @@
 package com.gitlab.nadiatrf.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan("com.gitlab.nadiatrf")
 @EnableWebMvc
 public class AppConfig {
+    @Bean
+    public DataSource mysqlDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.postgresql.jdbc.Driver");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/bank");
+        dataSource.setUsername("root");
+        dataSource.setPassword("root");
+
+        return dataSource;
+    }
 }
